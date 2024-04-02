37e49c2562bcb2f5371bb7d8150423cda6434dba
= <nmkubeka@gmail.com>
2024-04-03 00:43:24 +0200

    minor tweaks, add todays workout to project page

--- src/app/landing/hero/hero.component.ts
+++ src/app/landing/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { TechStackListComponent } from '../tech-stack-list/tech-stack-list.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [TechStackListComponent],
+  imports: [TechStackListComponent, RouterModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })

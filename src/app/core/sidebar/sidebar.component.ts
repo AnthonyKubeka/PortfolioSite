4fa517bb8e271a4f5901c3371e47fb46fc531ef3
= <nmkubeka@gmail.com>
2024-04-21 21:40:39 +0200

    add dark mode button on sidebar

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,17 +1,30 @@
+import { ThemeService } from './../../shared/theme.service';
+import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, EventEmitter, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { ionBulbOutline, ionBulbSharp } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, NgIconComponent],
+  providers: [provideIcons({ionBulbOutline, ionBulbSharp})],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   @Output() showSidebar = new EventEmitter<boolean>();
+  isDarkModeOn = false;
+  constructor(private themeService: ThemeService){}
+
 toggleSidebar() {
     this.showSidebar.emit(true);
+}
+
+toggleDarkMode(){
+  this.isDarkModeOn = !this.isDarkModeOn;
+  this.themeService.toggleDarkMode();
 }
 }
